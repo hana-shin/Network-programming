ba17137881bcfbbdd46f2d58df06a147dafcefc5
hana-shin <xxx@example.com>
2019-06-16 21:51:15 +0900

    modify

--- select/sv.c
+++ select/sv.c
@@ -32,33 +32,25 @@ int main()
     char* message = "Hello Client";
     void sig_chld(int);
 
-    /* create listening TCP socket */
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
 
-    // binding server addr structure to listenfd
     bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
     listen(listenfd, 10);
     
-        /* create UDP socket */
     udpfd = socket(AF_INET, SOCK_DGRAM, 0);
-    // binding server addr structure to udp sockfd
     bind(udpfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
-    // clear the descriptor set
     FD_ZERO(&rset);
 
-    // get maxfd
     maxfdp1 = max(listenfd, udpfd) + 1;
     for (;;) {
-        // set listenfd and udpfd in readset
         FD_SET(listenfd, &rset);
         FD_SET(udpfd, &rset);
 
-        // select the ready descriptor
         select(maxfdp1, &rset, NULL, NULL, NULL);
 
         if (FD_ISSET(listenfd, &rset)) {
@@ -88,4 +80,3 @@ int main()
                     }
     }
 }
-            
