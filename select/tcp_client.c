63fb0efc9168c51063126682946f38ea4d834677
hana-shin <xxx@example.com>
2019-06-17 20:36:01 +0900

    modify

--- select/tcp_client.c
+++ select/tcp_client.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include "def.h"
 
--- select/udp_client.c
+++ select/udp_client.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include "def.h"
 
