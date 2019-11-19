4527114ca2011310c649a9bfee08f0d5faf41772
hana_shin <hana-shin@users.noreply.github.com>
2019-11-19 20:25:31 +0900

    Update cl.c

--- socket/linger/cl.c
+++ socket/linger/cl.c
@@ -2,38 +2,29 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in server;
     int sock;
-    ssize_t n;
-    char buf[32]="0123456789";
+    struct sockaddr_in server;
+    struct linger lin;
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
     server.sin_family = AF_INET;
     server.sin_port = htons(11111);
-    server.sin_addr.s_addr = inet_addr("192.168.0.20");
+    server.sin_addr.s_addr = inet_addr("192.168.3.10");
 
-    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) == -1){
-        perror("connect");
-        _exit(1);
-    }
+    lin.l_onoff = 1;
+    lin.l_linger = 3;
+    setsockopt(sock, SOL_SOCKET, SO_LINGER, &lin, sizeof(lin));
 
-    while(1) {
-        n = write(sock, buf, sizeof(buf));
-        if( n == -1) {
-            perror("write");
-            _exit(1);
-        }
-        fprintf(stderr,"%zd, %s\n", n, buf);
-        sleep(5);
-    }
+    connect(sock, (struct sockaddr *)&server, sizeof(server));
+
+    write(sock, "123", sizeof("123"));
+    sleep(10);
+
     close(sock);
-    _exit(0);
+    return 0;
 }
