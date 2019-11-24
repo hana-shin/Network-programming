40a57699799afff05803841d14cd78cd3de91165
hana_shin <hana-shin@users.noreply.github.com>
2019-11-24 20:55:36 +0900

    Update cl.c

--- socket/nodelay/cl.c
+++ socket/nodelay/cl.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
+#include <netinet/tcp.h>
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in server;
-    int sock;
-    ssize_t n;
-    char buf[32]="0123456789";
+    int sock,val;
+    char buf[10]="0123456789";
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -17,15 +16,16 @@ int main(int argc, char *argv[])
     server.sin_port = htons(11111);
     server.sin_addr.s_addr = inet_addr("192.168.3.10");
 
+    val = atoi(argv[1]);
+    setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val));
     connect(sock, (struct sockaddr *)&server, sizeof(server));
 
-    while(1) {
-        n = write(sock, buf, sizeof(buf));
-        if(n == -1) {
-            perror("write");
-            return 1;
-        }
-    }
+    write(sock, buf, sizeof(buf));
+    write(sock, buf, sizeof(buf));
+    write(sock, buf, sizeof(buf));
+    write(sock, buf, sizeof(buf));
+    write(sock, buf, sizeof(buf));
+
     close(sock);
     return 0;
 }
