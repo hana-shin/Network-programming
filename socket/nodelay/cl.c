77f5f7d73aa49b49c7e19d0f20a5ce311df609b3
hana_shin <hana-shin@users.noreply.github.com>
2019-11-24 21:13:04 +0900

    Update cl.c

--- socket/nodelay/cl.c
+++ socket/nodelay/cl.c
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     struct sockaddr_in server;
-    int sock,val;
+    int sock,i,val;
     char buf[10]="0123456789";
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -17,14 +17,18 @@ int main(int argc, char *argv[])
     server.sin_addr.s_addr = inet_addr("192.168.3.10");
 
     val = atoi(argv[1]);
+    if(val==1)
+        printf("TCP_NODELAY option is ON\n");
+    else
+        printf("TCP_NODELAY option is OFF\n");
+
     setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val));
     connect(sock, (struct sockaddr *)&server, sizeof(server));
 
-    write(sock, buf, sizeof(buf));
-    write(sock, buf, sizeof(buf));
-    write(sock, buf, sizeof(buf));
-    write(sock, buf, sizeof(buf));
-    write(sock, buf, sizeof(buf));
+
+    for(i=0; i< 10; i++) {
+        write(sock, buf, sizeof(buf));
+    }
 
     close(sock);
     return 0;
