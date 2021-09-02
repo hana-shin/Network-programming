e095410770d931c951273dddb34af9a04815d639
hana-shin <xxx@example.com>
2021-09-02 21:37:25 +0900

    Modify

--- netcat/nc-poll.c
+++ netcat/nc-poll.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <poll.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[])
 {
   int sock_fd;
-  fd_set readfds;
   ssize_t n;
   char buf[4096];
   struct sockaddr_in server;
@@ -30,19 +26,15 @@ int main(int argc, char *argv[])
   pollFD = calloc(2, sizeof(struct pollfd));
   pollFD[0].fd = 0;
   pollFD[0].events = POLLIN;
-
+  pollFD[1].fd = sock_fd;
+  pollFD[1].events = POLLIN;
 
   connect(sock_fd, (struct sockaddr *)&server, sizeof(server));
 
-
-
   for(;;) {
-    FD_ZERO(&readfds);
-    FD_SET(0, &readfds);
-    FD_SET(sock_fd, &readfds);
+    poll(pollFD, 2, -1);
 
-    select(sock_fd+1, &readfds, NULL, NULL, NULL);
-    if(FD_ISSET(0, &readfds)) {
+    if(pollFD[0].revents & POLLIN) {
       if((n=read(0, buf, sizeof(buf))) < 0) {
         perror("read");
         return 1;
@@ -53,7 +45,7 @@ int main(int argc, char *argv[])
       write(sock_fd, buf, n);
     }
 
-    if(FD_ISSET(sock_fd, &readfds)) {
+    if(pollFD[sock_fd].revents & POLLIN) {
       if((n=read(sock_fd, buf, sizeof(buf))) < 0) {
         perror("read");
         return 1;
